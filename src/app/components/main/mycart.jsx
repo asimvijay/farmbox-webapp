2ae5446e7ea3eb2164f740ff905a3ce050b76eb0
Hashim Hasan <hashimhasan444@gmail.com>
2025-04-17 11:12:38 +0500

    .

--- src/app/components/main/mycart.jsx
+++ src/app/components/main/mycart.jsx
@@ -77,7 +77,7 @@ const MyCart = ({ cart, onClose, onCheckout }) => {
                     </div>
                   </div>
                 </div>
-              ))}
+              ))} 
               
               <div className="border-t border-gray-200 pt-4 mt-4">
                 <div className="flex justify-between items-center mb-4">
